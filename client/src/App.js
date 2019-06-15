a146ce5b0f344898fead2990db4476b781debb34
jsyum <jennifersyum@gmail.com>
2019-06-15 13:59:31 -0400

    Reformatted as LoginButton component

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,13 @@ import React, { Component } from "react";
 import Info from "./Containers/Info/Info";
 import WebScraper from "./Containers/web-scraper/web-scraper";
 import "./App.css";
-import Auth from "./Utils/Auth";
-
-function login() {
-  const auth = new Auth();
-  auth.login();
-}
+import LoginButton from "./components/LoginButton";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <button onClick={login}>Login button</button>
+        <LoginButton />
         <WebScraper />
       </div>
     );
