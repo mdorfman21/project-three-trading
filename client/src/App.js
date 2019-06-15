ceab3022beaef7c9aa6e5e6ce013ca8899ca1b32
jsyum <jennifersyum@gmail.com>
2019-06-15 13:47:15 -0400

    login button

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 import Info from "./Containers/Info/Info";
 import WebScraper from "./Containers/web-scraper/web-scraper";
 import "./App.css";
+import Auth from "./Utils/Auth";
+
+function login() {
+  const auth = new Auth();
+  auth.login();
+}
 
 class App extends Component {
   render() {
     return (
       <div>
+        <button onClick={login}>Login button</button>
         <WebScraper />
       </div>
     );
