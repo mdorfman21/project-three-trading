b738185ac08dcbeb0d78fdce67b0fb1be991b6d8
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-20 20:08:03 -0400

    commented out homepage component and import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Info from "./Containers/Info/Info";
 import WebScraper from "./Containers/web-scraper/web-scraper";
-import HomePage from "./Containers/HomePage/HomePage";
+// import HomePage from "./Containers/HomePage";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        {/* <HomePage /> */}
         <LoginButton />
         <WebScraper />
       </div>
