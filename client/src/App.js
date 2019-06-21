47ff47185ab28582de4ba9cd4aaf821f8c48141f
Morgan Dorfman <mdorfman21@gmail.com>
2019-06-20 20:12:14 -0400

    added homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Info from "./Containers/Info/Info";
 import WebScraper from "./Containers/web-scraper/web-scraper";
-
+import Homepage from "./Containers/HomePage/HomePage";
 import Pairs from "./Containers/Trading-strats/Pairs";
 
 import "./App.css";
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <HomePage />
         <Pairs />
 
         <LoginButton />
