be39c2da9f8c0462724c3e5be634f177957dd181
Eddie Jaquez <erjaquez@gmail.com>
2019-06-22 13:26:45 -0400

    no changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,113 @@
-import React, { Component } from "react";
-import Info from "./Containers/Info/Info";
-import WebScraper from "./Containers/web-scraper/web-scraper";
-import Homepage from "./Containers/HomePage/HomePage";
-import Pairs from "./Containers/Trading-strats/Pairs";
+// import React, { Component } from "react";
+// import Info from "./Containers/Info/Info";
+// import WebScraper from "./Containers/web-scraper/web-scraper";
+// import Homepage from "./Containers/HomePage/HomePage";
+// import Pairs from "./Containers/Trading-strats/Pairs";
 
-import "./App.css";
-import LoginButton from "./components/LoginButton";
+// import "./App.css";
+// import LoginButton from "./components/LoginButton";
+
+// class App extends Component {
+  
+
+//   render() {
+//     return (
+//       <div>
+//         <Homepage />
+//         <Pairs />
+
+//         <LoginButton />
+//         <WebScraper />
+//         {/* <Info /> */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+
     return (
       <div>
-        <Homepage />
-        <Pairs />
-
-        <LoginButton />
-        <WebScraper />
-        {/* <Info /> */}
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                <Button
+                  id="qsLoginBtn"
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                  </Button>
+              )
+            }
+            {
+              isAuthenticated() && (
+                <Button
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.goTo.bind(this, 'profile')}
+                >
+                  Profile
+                  </Button>
+              )
+            }
+            {
+              isAuthenticated() && (
+                <Button
+                  id="qsLogoutBtn"
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.logout.bind(this)}
+                >
+                  Log Out
+                  </Button>
+              )
+            }
+          </Navbar.Header>
+        </Navbar>
+        <div className="container">
+          {this.props.children}
+        </div>
       </div>
     );
   }
