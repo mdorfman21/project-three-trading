118b430a04f3bf06cec19bdb2149788e541afe36
Morgan Dorfman <mdorfman21@gmail.com>
2019-07-09 13:01:02 -0400

    test

--- scripts/fakeSeeed.js
+++ scripts/fakeSeeed.js
@@ -70,7 +70,7 @@ givenPairsData.find().then(dbStock => {
 
   console.log(testArray.slice(0, -1900));
   testArray.slice(0, -1950).forEach(stock => {
-    console.log(stock.stats.length);
+    console.log(stock.stats.CoreMongooseArray.length);
     if (stock.stats.length > 0) {
       testArray.forEach(otherStock => {
         console.log(
