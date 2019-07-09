3bd1683caf4f1684e24128c3f12c78031bd2022b
Morgan Dorfman <mdorfman21@gmail.com>
2019-07-09 12:17:44 -0400

    test

--- scripts/fakeSeeed.js
+++ scripts/fakeSeeed.js
@@ -68,7 +68,7 @@ givenPairsData.find().then(dbStock => {
     return { symbol: obj.symbol, stats: stats };
   });
 
-  testArray.forEach(stock => {
+  testArray.slice(0, -1900).forEach(stock => {
     console.log(stock.stats.length);
     if (stock.stats.length > 0) {
       testArray.forEach(otherStock => {
