602b33919af1a0eeec930881cfe8cd36ba0ce1e1
Morgan Dorfman <mdorfman21@gmail.com>
2019-07-10 18:01:34 -0400

    spelling mistake fixed

--- client/src/Containers/Info/Info.js
+++ client/src/Containers/Info/Info.js
@@ -63,142 +63,142 @@ class Info extends React.Component {
 
     return (
       <div>
-      <Container>
+        <Container>
+          <br />
+          <Row>
+            <Col>
+              <h1 className="font-title">Single Stock Info</h1>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col className="formButtons">
+              <Form
+                name="search"
+                onChange={this.updateSearch}
+                placeholder="Stock Ticker"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col className="formButtons">
+              <Button
+                name="Bollinger Bands"
+                onClick={this.getStockInfo}
+                className="btn btn-success"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col className="formButtons">
+              <Button
+                name="Stock Statistics"
+                onClick={this.getStockStats}
+                className="btn btn-success"
+              />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <p className="font">
+                Here we have a graph showing just one securities' bollinger
+                bands. This is to just get some more information about the
+                security of your choosing.
+              </p>
+              <p className="font">
+                The bollinger bands are two standard deviations away from the
+                moving average. Typically, the price of the security oscilates
+                between these bands. When the price breaks this band in either
+                direction and continues in the same direction, that security may
+                be heading for a breakout in that direction.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <StockInfoChart
+                categories={this.state.stockDays}
+                dataOne={this.state.stockInfoArray}
+                stockOne={this.state.search}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="font">
+                Below we have key financial statistics about the security you
+                are searching for. These can be used for indicators for certain
+                trading strategies.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="stockInfoDiv">
+                {statsArray.stats.length > 0
+                  ? optionOne.map((stat, index) => (
+                      <span className="stockInfoSpan">
+                        <StockInfo
+                          name={stat.name}
+                          value={stat.value}
+                          key={stat.name}
+                        />
+                      </span>
+                    ))
+                  : ""}
+              </div>
+            </Col>
+            <Col>
+              <div className="stockInfoDiv">
+                {statsArray.stats.length > 0
+                  ? optionTwo.map((stat, index) => (
+                      <span className="stockInfoSpan">
+                        <StockInfo
+                          name={stat.name}
+                          value={stat.value}
+                          key={stat.name}
+                        />
+                      </span>
+                    ))
+                  : ""}
+              </div>
+            </Col>
+            <Col>
+              <div className="stockInfoDiv">
+                {statsArray.stats.length > 0
+                  ? optionThree.map((stat, index) => (
+                      <span className="stockInfoSpan">
+                        <StockInfo
+                          name={stat.name}
+                          value={stat.value}
+                          key={stat.name}
+                        />
+                      </span>
+                    ))
+                  : ""}
+              </div>
+            </Col>
+            <Col>
+              <div className="stockInfoDiv">
+                {statsArray.stats.length > 0
+                  ? optionFour.map((stat, index) => (
+                      <span className="stockInfoSpan">
+                        <StockInfo
+                          name={stat.name}
+                          value={stat.value}
+                          key={stat.name}
+                        />
+                      </span>
+                    ))
+                  : ""}
+              </div>
+            </Col>
+          </Row>
+        </Container>
         <br />
-        <Row>
-          <Col>
-            <h1 className="font-title">Single Stock Info</h1>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col className="formButtons">
-            <Form
-              name="search"
-              onChange={this.updateSearch}
-              placeholder="Stock Ticker"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col className="formButtons">
-            <Button
-              name="Bollinger Bands"
-              onClick={this.getStockInfo}
-              className="btn btn-success"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col className="formButtons">
-            <Button
-              name="Stock Statistics"
-              onClick={this.getStockStats}
-              className="btn btn-success"
-            />
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <p className="font">
-              Here we have a graph showing just one securities bollinger bands.
-              This is to just get some more information about the security of
-              your choosing.
-            </p>
-            <p className="font">
-              The bollinger bands are two standard deviations away from the
-              moving average. Typically, the price of the security oscilates
-              between these bands. When the price breaks this band in either
-              direction and continues in the same direction, that security may
-              be heading for a breakout in that direction.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <StockInfoChart
-              categories={this.state.stockDays}
-              dataOne={this.state.stockInfoArray}
-              stockOne={this.state.search}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="font">
-              Below we have key financial statistics about the security you are
-              searching for. These can be used for indicators for certain
-              trading strategies.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="stockInfoDiv">
-              {statsArray.stats.length > 0
-                ? optionOne.map((stat, index) => (
-                    <span className="stockInfoSpan">
-                      <StockInfo
-                        name={stat.name}
-                        value={stat.value}
-                        key={stat.name}
-                      />
-                    </span>
-                  ))
-                : ""}
-            </div>
-          </Col>
-          <Col>
-            <div className="stockInfoDiv">
-              {statsArray.stats.length > 0
-                ? optionTwo.map((stat, index) => (
-                    <span className="stockInfoSpan">
-                      <StockInfo
-                        name={stat.name}
-                        value={stat.value}
-                        key={stat.name}
-                      />
-                    </span>
-                  ))
-                : ""}
-            </div>
-          </Col>
-          <Col>
-            <div className="stockInfoDiv">
-              {statsArray.stats.length > 0
-                ? optionThree.map((stat, index) => (
-                    <span className="stockInfoSpan">
-                      <StockInfo
-                        name={stat.name}
-                        value={stat.value}
-                        key={stat.name}
-                      />
-                    </span>
-                  ))
-                : ""}
-            </div>
-          </Col>
-          <Col>
-            <div className="stockInfoDiv">
-              {statsArray.stats.length > 0
-                ? optionFour.map((stat, index) => (
-                    <span className="stockInfoSpan">
-                      <StockInfo
-                        name={stat.name}
-                        value={stat.value}
-                        key={stat.name}
-                      />
-                    </span>
-                  ))
-                : ""}
-            </div>
-          </Col>
-        </Row>
-      </Container>
-        <br></br>
         <Footer />
-        </div>
+      </div>
     );
   }
 }
